fea7987f1d04b19f45b02f9501484ee1593accd4
alanorlando.cruz.manrique <alanorlando.cruz.manrique@bbva.com>
2017-05-22 13:01:30 -0500

    upload seemas-adapter-bridge

---
+++ index.js
@@ -0,0 +1,43 @@
+'use strict';
+
+
+let express = require('express');
+let app = express();
+
+let osprey = require('osprey');
+let path   = require('path');
+let raml   = path.join(__dirname, 'rsc', 'adapter-bridge-api.raml');
+let times  = require(path.join(__dirname, '/lib/times'));
+let utils  = require(path.join(__dirname, '/lib/utils'));
+let raml2html  = require(path.join(__dirname, '/lib/raml2html'));
+
+
+osprey.loadFile(raml)
+  .then(function (middleware) {
+    console.log("//////// SERVER START IN PORT 3000/////////");
+    app.use(middleware)
+
+    app.get('/', function(req, res){
+      var response = {status: "OK"};
+      raml2html.getHtmlRaml(function(result){
+        console.log("RESULT :", JSON.stringify(result));
+        if(result.status === "OK"){
+          res.send(result);
+        } else {
+          res.send(response);
+        }
+      });
+    });
+    app.post('/event', function(req, res){
+      var response = {};
+      console.log("REQUEST : :", req.body );
+      var now = times.getCurrentTime();
+      response.id = req.body.id;
+      response.transactionTime = now.format(utils.DATETIME_FORMAT);
+      res.send(response);
+    });
+    app.listen(3000)
+  })
+  .catch(function(e) {
+    console.error("ERROR : %s", e.message);
+  });
